5d17d3db3a52789e3d1ee7f21af7647b7acc4346
Marissa Staller <marissastaller@Marissas-MacBook-Pro.local>
2016-10-06 08:36:52 -0400

    portfolio pages

---
+++ startbootstrap-landing-page-gh-pages/js/landing-page.js
@@ -0,0 +1,41 @@
+/*! Marissa Staller
+ * Bootstrap v3.3.7 (http://getbootstrap.com)
+ * Copyright 2011-2016 Twitter, Inc.
+ * Licensed under the MIT license
+ */
+$(document).ready(function(){
+	$('#s3').cycle({ 
+    	fx:    'fade', 
+    	speed:  1500 
+ 	});
+
+$('#GoogleSite').click(function(){
+ 		window.open("https://sites.google.com/site/mstallerscience/");
+ 	});
+
+/*!
+ 	$('#legoDUPLO').click(function(){
+ 		window.open("http://codepen.io/jhandshoe/pen/rLmgrr");
+ 	});
+
+ 	$('#fizzBuzz').click(function(){
+ 		window.open("http://ajax-tutorial.herokuapp.com/");
+ 	});
+
+ 	$('#Calculator').click(function(){
+ 		window.open("https://meditate-app.herokuapp.com/");
+ 	});
+
+ 	$('#Gradebook').click(function(){
+ 		window.open("http://mean-unmultipliable-armpit.mybluemix.net/");
+ 	});
+
+ 	$('#Blackjack').click(function(){
+ 		window.open("https://workoutweb-1150-jhandshoe.herokuapp.com/");
+ 	});
+
+ 	
+
+ 	
+ 	*/
+});
