176873b66a799760e6494507069c80169205b147
Marissa Staller <mardisworld@gmail.com>
2016-11-19 03:20:14 -0500

    Portfolio at 9 Week gradation

--- js/landing-page.js
+++ js/landing-page.js
@@ -9,35 +9,55 @@ $(document).ready(function(){
     	speed:  1500 
  	});
 
-$('#GoogleSite').click(function(){
+
+	/*#1. Calculator*/
+	$('#Calculator').click(function(){
+ 		window.open("https://www.repl.it/EWt2/2");
+ 	});
+
+	/*#2. Gradebook*/
+ 	$('Gradebook').click(function(){
+ 		window.open("https://www.repl.it/DqIX/2");
+ 	});
+	
+	/*#3. Blackjack"*/
+ 	$('#Blackjack').click(function(){
+ 		window.open("https://www.repl.it/E1Qu/5");
+ 	});
+ 	
+ 	/*#4. GoogleSite"*/
+ 	$('#GoogleSite').click(function(){
  		window.open("https://sites.google.com/site/mstallerscience/");
  	});
-
-
- 	$('#legoDUPLO').click(function(){
+ 	
+ 	/*#5.  Duplo Inventory"*/
+ 	$('#Duplo').click(function(){
  		window.open("DUPLOstore/storepage.html");
  	});
+ 	
+ 	/*#6.  Smoothie"*/
+ 	$('#Smoothie').click(function(){
+ 		window.open("https://github.com/mardisworld/SmoothieShop");
+ 	});
 
- 	$('#fizzBuzz').click(function(){
- 		window.open("https://repl.it/Dpwa/3");
+ 	/*#7.Employee App"*/
+ 	$('#Employee').click(function(){
+ 		window.open("https://marissa-employee-app.herokuapp.com/");
  	});
- 	/*
+ 	
+ 	/*#8. Inventory-"*/
+ 	$('#Inventory').click(function(){
+ 		window.open("https://duplostore.herokuapp.com");
+ 	});
+ 	
+ 	/*#9.  CRM Social Users*/
+ 	$('#CRMSocialMediaUser').click(function(){
+ 		window.open(" https://mardisworld-crm-app.herokuapp.com/");
+ 	});
 
- 	$('#Calculator').click(function(){
- 		window.open("https://meditate-app.herokuapp.com/");
- 	});
- 	*/
-
- 	$('#Gradebook').click(function(){
- 		window.open("https://repl.it/DqIX/2");
- 	});
-/*
- 	$('#Blackjack').click(function(){
- 		window.open("https://workoutweb-1150-jhandshoe.herokuapp.com/");
- 	});
 
  	
 
  	
- 	*/
+ 	
 });
